442eb9964b9c37745882c3e5a79c9c1676f3e15f
Aditya Rathi <aditya.rathi9555@gmail.com>
2019-02-24 21:26:39 +0530

    changed for local

--- server/config/config.js
+++ server/config/config.js
@@ -3,14 +3,14 @@ var env=process.env.NODE_ENV || 'development';
 if(env === 'test')
 {
     //For aws
-    process.env.PORT=80;
+//     process.env.PORT=80;
     //For local
-//     process.env.PORT=3000;
+    process.env.PORT=3000;
     process.env.MONGODB_URI='mongodb://localhost:27017/VShoppingTest'
 }else if(env === 'development')
 {
-    rocess.env.PORT=80;
+//     process.env.PORT=80;
     //For local
-//     process.env.PORT=3000;
+    process.env.PORT=3000;
     process.env.MONGODB_URI='mongodb://localhost:27017/VShopping'
 }
